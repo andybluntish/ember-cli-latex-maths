880b08cb402e0cdac54abc48089cf168285ca611
Simon Booth <simon@74monkeys.com>
2016-02-08 16:08:53 +0100

    We need an entity name

--- blueprints/ember-cli-latex-maths/index.js
+++ blueprints/ember-cli-latex-maths/index.js
@@ -1,5 +1,6 @@
 module.exports = {
   //description: '',
+  normalizeEntityName: function() {}, // no-op since we're just adding dependencies
 
   // locals: function(options) {
   //   // Return custom template variables here.
