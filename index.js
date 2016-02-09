71a1b9a5949b184fd1679e9c569165d8c16c7f47
Andy Bluntish <andy@bluntish.net>
2016-02-09 20:31:31 +1100

    Add blueprint to install bower dependency

--- index.js
+++ index.js
@@ -1,6 +1,25 @@
 /* jshint node: true */
 'use strict';
 
+var fs = require('fs');
+var path = require('path');
+
 module.exports = {
-  name: 'ember-cli-latex-maths'
+  name: 'ember-cli-latex-maths',
+
+  included: function(app) {
+    this._super.included(app);
+
+    var libPath = path.join(app.bowerDirectory, 'KaTeX', 'dist');
+    var fontPath = path.join(libPath, 'fonts');
+
+    app.import(path.join(libPath, 'katex.min.css'));
+    app.import(path.join(libPath, 'katex.min.js'));
+
+    fs.readdirSync(fontPath).forEach(function(font) {
+      app.import(path.join(fontPath, font), {
+        destDir: path.join('assets', 'fonts')
+      });
+    });
+  }
 };
